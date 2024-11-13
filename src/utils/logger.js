f1ace69c7600d2a14623be3e2793ee622f957b08
rittikbasu <irittik@gmail.com>
2024-11-13 10:54:59 +0530

    refactor: only show console logs in debug mode

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,6 +2,6 @@ import { DEBUG } from "../config/constants";
 
 export const log = {
   debug: (...args) => DEBUG && console.log("[DEBUG]", ...args),
-  error: (...args) => console.error("[ERROR]", ...args),
-  warn: (...args) => console.warn("[WARN]", ...args),
+  error: (...args) => DEBUG && console.error("[ERROR]", ...args),
+  warn: (...args) => DEBUG && console.warn("[WARN]", ...args),
 };
